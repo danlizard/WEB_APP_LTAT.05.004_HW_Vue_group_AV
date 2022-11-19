161c6f0ea2be71be10bdeca550973d327216450f
danlizard <dan.kuklyanov@gmail.com>
2022-11-19 16:18:20 +0200

    midport of old design

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <NavBar/>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <RouterView/>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Footer/>
 </template>
 
 <style scoped>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+<header>
+  <div class="navigation_bar">
+    <button>
+        Home
+    </button>
+    <button>
+        Signup
+    </button>
+    <a id="myLink" href="#" style="padding: 0px;">
+		<img class="profile_pic" src="/profile2.png" alt="Your profile picture">
+		    <div id="myDropdown" class="dropdown-content">
+				<a href="#" id="myDropdownName">Link 1</a>
+				<a href="#" id="myDropdownEmail">Link 2</a>
+				<a href="#" id="myDropdownLogout">Link 3</a>
+			</div>
+	</a>
+  </div>
+</header>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
