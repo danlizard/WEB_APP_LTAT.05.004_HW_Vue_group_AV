57e53e257beb95f83185cc0c4b2752d4d623cdb7
aleksandr.makarov <aleksandr.makarov@ut.ee>
2022-11-20 18:51:48 +0300

    Footer and auto refresh

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,11 +3,18 @@
 
 <template>
     <footer>
-        <div class="bottom_bar"></div>
+        <div class="bottom_bar">
+          <p>Ⓒ 2022 University of Tartu. The website is developed for LTAT.05.004 course</p>
+        </div>
     </footer>
 </template>
 
 <style scoped>
+p {
+  font-size: 2vh;
+  color: aquamarine;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
